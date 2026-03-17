720efb6a15c224256954910d7444063a42de82ef
shaheryarbhatti <shaheryar_b@yahoo.com>
2026-03-17 13:16:56 +0500

    created attendance module

--- resources/views/attendance/manage.blade.php
+++ resources/views/attendance/manage.blade.php
@@ -1,5 +1,16 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Sky Fitness Gym – premium gym portal.">
+    <meta name="author" content="skyfitnessgym.com">
+    <link rel="icon" href="{{ asset('public/assets/images/favicon.png')}}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('public/assets/images/favicon.png')}}" type="image/x-icon">
+    <title>{{ __('attendance_logs') }}</title>
+@extends('layouts.app')
 @section('content')
 <style>
     #dateFilter {
